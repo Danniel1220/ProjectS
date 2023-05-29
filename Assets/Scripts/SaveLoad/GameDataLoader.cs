95f16f455d50204a3fa274a32daec1252fc079e7
Clim Daniel George <climdanniel@gmail.com>
2023-05-30 02:23:15 +0300

    Moved prefabs folder to resources folder for easier access via code

--- Assets/Scripts/SaveLoad/GameDataLoader.cs
+++ Assets/Scripts/SaveLoad/GameDataLoader.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameDataLoader : MonoBehaviour
+public static class GameDataLoader
 {
-    // Start is called before the first frame update
-    void Start()
+    public static void loadGameData()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
---
+++ Assets/Scripts/StarPrefabs.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class StarPrefabs
+{
+    public static GameObject classOStar = Resources.Load("Prefabs/O Class Star") as GameObject;
+    public static GameObject classBStar = Resources.Load("Prefabs/B Class Star") as GameObject;
+    public static GameObject classAStar = Resources.Load("Prefabs/A Class Star") as GameObject;
+    public static GameObject classFStar = Resources.Load("Prefabs/F Class Star") as GameObject;
+    public static GameObject classGStar = Resources.Load("Prefabs/G Class Star") as GameObject;
+    public static GameObject classKStar = Resources.Load("Prefabs/K Class Star") as GameObject;
+    public static GameObject classMStar = Resources.Load("Prefabs/M Class Star") as GameObject;
+}
