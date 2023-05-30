cb291e86f1f0c45d2302260fe45b778066368f75
Clim Daniel George <climdanniel@gmail.com>
2023-05-30 22:15:00 +0300

    Main menu looks implemented

--- Assets/AutoPlanetGenerationTestScript.cs
+++ Assets/AutoPlanetGenerationTestScript.cs
@@ -9,8 +9,7 @@ public class AutoPlanetGenerationTestScript : MonoBehaviour
     {
         this.gameObject.GetComponent<PlanetFactory>().generatePlanet(this.transform, 0f);
         this.transform.GetChild(0).gameObject.SetActive(true);
-        GameObject planet = this.transform.GetChild(8).gameObject;
-        planet.GetComponent<PlanetGenerationSettings>().shapeSettings.planetRadius = 400f;
+        this.transform.GetChild(0).gameObject.GetComponent<PlanetGenerationSettings>().shapeSettings.planetRadius = 400f;
     }
 
     // Update is called once per frame
