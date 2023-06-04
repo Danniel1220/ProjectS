afa15a6d8814cb3f4a6380863520088a5557ad06
Clim Daniel George <climdanniel@gmail.com>
2023-06-04 03:11:00 +0300

    Working on the planet menu UI and changed the main font used to a custom one thats more fitting

--- Assets/Scripts/UI/UIManagers.cs
+++ Assets/Scripts/UI/UIManagers.cs
@@ -4,16 +4,12 @@ using UnityEngine;
 
 public class UIManagers : MonoBehaviour
 {
-    public static CanvasManager canvasManager;
-
     public static PlanetInfoPanel planetInfoPanel;
     public static PlanetMenuPanel planetMenuPanel;
 
 
     void Awake()
     {
-        canvasManager = GameObject.Find("Canvas").GetComponent<CanvasManager>();
-
         planetInfoPanel = GameObject.Find("Canvas").transform.Find("PlanetInfo").GetComponent<PlanetInfoPanel>();
         planetMenuPanel = GameObject.Find("Canvas").transform.Find("PlanetMenu").GetComponent<PlanetMenuPanel>();
     }
