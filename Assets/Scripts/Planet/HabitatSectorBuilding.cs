5a4f58698a2d94d97f4270f84b35b1b024c2cd7b
Clim Daniel George <climdanniel@gmail.com>
2023-06-04 16:42:41 +0300

    Started to implement the sector/buildings logic for planets

---
+++ Assets/Scripts/Planet/HabitatSectorBuilding.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HabitatSectorBuilding
+{
+    public HabitatBuildingType buildingType;
+    public int currentBuildingPopulation;
+    public int maxBuildingPopulation;
+
+    public enum HabitatBuildingType { SmallSettlement, Base, City };
+
+    public HabitatSectorBuilding(HabitatBuildingType buildingType, int currentBuildingPopulation)
+    {
+        this.buildingType = buildingType;
+        this.currentBuildingPopulation = currentBuildingPopulation;
+
+        // assigning the max population depending on what type of habitat building this is
+        switch (buildingType)
+        {
+            case HabitatBuildingType.SmallSettlement:
+                maxBuildingPopulation = 10;
+                break;
+            case HabitatBuildingType.Base:
+                maxBuildingPopulation = 100;
+                break;
+            case HabitatBuildingType.City:
+                maxBuildingPopulation = 100;
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Planet/Sector.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sector
+{
+    public SectorType type;
+
+    public enum SectorType { Habitat, Storage, Energy, Mining, Production, Science};
+}
