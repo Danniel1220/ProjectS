e899dce724eb3a75c4ebf7724863c479e728ea65
Clim Daniel George <climdanniel@gmail.com>
2024-02-06 02:34:29 +0200

    Sepparating noise of X and Z axis

--- Assets/Scripts/Factory/GalaxyFactory.cs
+++ Assets/Scripts/Factory/GalaxyFactory.cs
@@ -48,24 +48,28 @@ public class GalaxyFactory : MonoBehaviour
         public float turnFraction;
         public float turnOffset;
         public float distanceFactor;
-        public float locationNoiseXZ;
+        public float locationNoiseX;
         public float locationNoiseY;
-        public float minLocationNoiseXZ;
+        public float locationNoiseZ;
+        public float minLocationNoiseX;
         public float minLocationNoiseY;
+        public float minLocationNoiseZ;
         public bool decreaseXZNoiseByDistance;
         public bool decreaseYNoiseByDistance;
         public CubeColor cubeColor;
 
-        public DiskSettings(int numberOfPoints, float turnFraction, float turnOffset, float distanceFactor, float locationNoiseXZ, float locationNoiseY, float minLocationNoiseXZ, float minLocationNoiseY, bool decreaseXZNoiseByDistance, bool decreaseYNoiseByDistance, CubeColor cubeColor)
+        public DiskSettings(int numberOfPoints, float turnFraction, float turnOffset, float distanceFactor, float locationNoiseX, float locationNoiseY, float locationNoiseZ, float minLocationNoiseX, float minLocationNoiseY, float minLocationNoiseZ, bool decreaseXZNoiseByDistance, bool decreaseYNoiseByDistance, CubeColor cubeColor)
         {
             this.numberOfPoints = numberOfPoints;
             this.turnFraction = turnFraction;
             this.turnOffset = turnOffset;
             this.distanceFactor = distanceFactor;
-            this.locationNoiseXZ = locationNoiseXZ;
+            this.locationNoiseX = locationNoiseX;
             this.locationNoiseY = locationNoiseY;
-            this.minLocationNoiseXZ = minLocationNoiseXZ;
+            this.locationNoiseZ = locationNoiseZ;
+            this.minLocationNoiseX = minLocationNoiseX;
             this.minLocationNoiseY = minLocationNoiseY;
+            this.minLocationNoiseZ = minLocationNoiseZ;
             this.decreaseXZNoiseByDistance = decreaseXZNoiseByDistance;
             this.decreaseYNoiseByDistance = decreaseYNoiseByDistance;
             this.cubeColor = cubeColor;
@@ -89,177 +93,6 @@ public class GalaxyFactory : MonoBehaviour
         //StartCoroutine(primaryDiskCoroutine);
         //StartCoroutine(secondaryDiskCoroutine);
         //StartCoroutine(removeOverlappedStarSystemsCoroutine);
-    }
-
-    public IEnumerator createPointsOnDiskEnumerator(DiskSettings disk)
-    {
-        for (int i = 0; i < disk.numberOfPoints; i++)
-        {
-            // computing points on a circle with a specific turn fraction that forms a galaxy shape
-            float distance = i / (disk.numberOfPoints - 1f);
-            float angle = (float)(2 * Mathf.PI * disk.turnFraction * i);
-
-            float pointX = distance * Mathf.Cos(angle) * disk.distanceFactor;
-            float pointY = 0;
-            float pointZ = distance * Mathf.Sin(angle) * disk.distanceFactor;
-
-            float noiseX;
-            float noiseY;
-            float noiseZ;
-
-            // mapping the distance to center to the inverse of maximum location noise,
-            // this way the further away from the center a point is,
-            // we can decrease the location noise on any of the 3 axis, if required
-            float distanceToCenter = Vector3.Distance(new Vector3(pointX, 0, pointZ), Vector3.zero);
-            float mapInput = Mathf.InverseLerp(disk.distanceFactor, 0, distanceToCenter);
-            float mapOutputXZ = Mathf.Lerp(0, disk.locationNoiseXZ, mapInput);
-            float mapOutputY = Mathf.Lerp(0, disk.locationNoiseY, mapInput);
-
-            // clamping noises above a set parameter
-            if (mapOutputXZ < disk.minLocationNoiseXZ) mapOutputXZ = disk.minLocationNoiseXZ;
-            if (mapOutputY < disk.minLocationNoiseY) mapOutputY = disk.minLocationNoiseY;
-
-            if (disk.decreaseXZNoiseByDistance)
-            {
-                noiseX = Random.Range(-mapOutputXZ, mapOutputXZ);
-                noiseZ = Random.Range(-mapOutputXZ, mapOutputXZ);
-            }
-            else
-            {
-                noiseX = Random.Range(-disk.locationNoiseXZ, disk.locationNoiseXZ);
-                noiseZ = Random.Range(-disk.locationNoiseXZ, disk.locationNoiseXZ);
-            }
-
-            if (disk.decreaseYNoiseByDistance)
-            {
-                noiseY = Random.Range(-mapOutputY, mapOutputY);
-            }
-            else
-            {
-                noiseY = Random.Range(-disk.locationNoiseY, disk.locationNoiseY);
-            }
-
-            float pointXAfterNoise = pointX + noiseX;
-            float pointYAfterNoise = pointY + noiseY;
-            float pointZAfterNoise = pointZ + noiseZ;
-
-            starFactory.createStarSystem(new Vector3(pointXAfterNoise, pointYAfterNoise, pointZAfterNoise));
-
-            // yield only every 100 stars
-            if (i % 100 == 0) yield return null;
-        }
-    }
-
-    public IEnumerator removeOverlappedStarSystemsEnumerator(float minDistance)
-    {
-        int checksMade = 0;
-        // for each chunk
-        foreach (Chunk chunk in chunkSystem.chunkList)
-        {
-            // iterate through every object in a given chunk
-            for (int i = 0; i < chunk.chunkGameObjectList.Count;)
-            {
-                // assume that we dont have to delete the object
-                bool shouldDelete = false;
-
-                // iterate again through every object in the given chunk
-                for (int j = 0; j < chunk.chunkGameObjectList.Count; j++)
-                {
-                    checksMade++;
-                    // if the objects are not one and the same and the distance between them is smaller than the minDistance
-                    // and if both objects have Star tags
-                    if (chunk.chunkGameObjectList[i] != chunk.chunkGameObjectList[j] &&
-                        Vector3.Distance(chunk.chunkGameObjectList[i].transform.position, chunk.chunkGameObjectList[j].transform.position) < minDistance &&
-                        chunk.chunkGameObjectList[i].tag == "Star System" && chunk.chunkGameObjectList[j].tag == "Star System")
-                    {
-                        // flag the object that it should be deleted
-                        shouldDelete = true;
-                        // break out of the loop since we already know the object should be deleted
-                        break;
-                    }
-                }
-                // if the object has been flagged for deletion
-                if (shouldDelete)
-                {
-                    // delete the object
-                    chunkSystem.deleteGameObjectFromChunk(chunk.chunkGameObjectList[i], chunk);
-                    // continue the loop without incrementing because an item has been removed from current index
-                    // so for the next loop we keep the index the same, but in reality the new object that is checked is the next one
-                    continue;
-                }
-                // we can only reach this statement if no object has been deleted, to continue with the iteration
-                i++;
-            }
-
-            // yield every chunk
-            yield return null;
-        }
-
-        Debug.Log("Checks made: " + checksMade);
-
-        galaxyGenerationTimeEnd = DateTime.Now;
-        galaxyGenerationTimeElapsed = galaxyGenerationTimeEnd - galaxyGenerationTimeStart;
-
-        Debug.Log("Galaxy Generation Time Elapsed: " + galaxyGenerationTimeElapsed);
-    }
-
-    public void createPointsOnDisk(DiskSettings disk)
-    {
-        for (int i = 0; i < disk.numberOfPoints; i++)
-        {
-            // computing points on a circle with a specific turn fraction that forms a galaxy shape
-            float distance = i / (disk.numberOfPoints - 1f);
-            float angle = (float)(2 * Mathf.PI * disk.turnFraction * i);
-
-            float pointX = distance * Mathf.Cos(angle) * disk.distanceFactor;
-            float pointY = 0;
-            float pointZ = distance * Mathf.Sin(angle) * disk.distanceFactor;
-
-            float noiseX;
-            float noiseY;
-            float noiseZ;
-
-            // mapping the distance to center to the inverse of maximum location noise,
-            // this way the further away from the center a point is,
-            // we can potentially decrease the location noise if required,
-            // on any of the 3 axis
-            float distanceToCenter = Vector3.Distance(new Vector3(pointX, 0, pointZ), Vector3.zero);
-            float mapInput = Mathf.InverseLerp(disk.distanceFactor, 0, distanceToCenter);
-            float mapOutputXZ = Mathf.Lerp(0, disk.locationNoiseXZ, mapInput);
-            float mapOutputY = Mathf.Lerp(0, disk.locationNoiseY, mapInput);
-
-            // clamping noises above a set parameter
-            if (mapOutputXZ < disk.minLocationNoiseXZ) mapOutputXZ = disk.minLocationNoiseXZ;
-            if (mapOutputY < disk.minLocationNoiseY) mapOutputY = disk.minLocationNoiseY;
-
-            if (disk.decreaseXZNoiseByDistance)
-            {
-                noiseX = Random.Range(-mapOutputXZ, mapOutputXZ);
-                noiseZ = Random.Range(-mapOutputXZ, mapOutputXZ);
-            }
-            else
-            {
-                noiseX = Random.Range(-disk.locationNoiseXZ, disk.locationNoiseXZ);
-                noiseZ = Random.Range(-disk.locationNoiseXZ, disk.locationNoiseXZ);
-            }
-
-            if (disk.decreaseYNoiseByDistance)
-            {
-                noiseY = Random.Range(-mapOutputY, mapOutputY);
-            }
-            else
-            {
-                noiseY = Random.Range(-disk.locationNoiseY, disk.locationNoiseY);
-            }
-
-            float pointXAfterNoise = pointX + noiseX;
-            float pointYAfterNoise = pointY + noiseY;
-            float pointZAfterNoise = pointZ + noiseZ;
-
-            starFactory.createStarSystem(new Vector3(pointXAfterNoise, pointYAfterNoise, pointZAfterNoise));
-
-            chunkSystem.updateDebugStarNumber();
-        }
     }
 
     public void removeOverlappedStarSystems(float minDistance)
@@ -329,6 +162,10 @@ public class GalaxyFactory : MonoBehaviour
             float pointY = 0;
             float pointZ = distance * Mathf.Sin(angle) * disk.distanceFactor;
 
+            //float pointX = distance * (Mathf.Cos(angle) + disk.turnFraction) * disk.distanceFactor;
+            //float pointY = 0;
+            //float pointZ = distance * (Mathf.Sin(angle) + disk.turnFraction) * disk.distanceFactor;
+
             float noiseX;
             float noiseY;
             float noiseZ;
@@ -339,22 +176,23 @@ public class GalaxyFactory : MonoBehaviour
             // on any of the 3 axis
             float distanceToCenter = Vector3.Distance(new Vector3(pointX, 0, pointZ), Vector3.zero);
             float mapInput = Mathf.InverseLerp(disk.distanceFactor, 0, distanceToCenter);
-            float mapOutputXZ = Mathf.Lerp(0, disk.locationNoiseXZ, mapInput);
+            float mapOutputX = Mathf.Lerp(0, disk.locationNoiseX, mapInput);
             float mapOutputY = Mathf.Lerp(0, disk.locationNoiseY, mapInput);
+            float mapOutputZ = Mathf.Lerp(0, disk.locationNoiseZ, mapInput);
+
 
             // clamping noises above a set parameter
-            if (mapOutputXZ < disk.minLocationNoiseXZ) mapOutputXZ = disk.minLocationNoiseXZ;
+            if (mapOutputX < disk.minLocationNoiseX) mapOutputX = disk.minLocationNoiseX;
             if (mapOutputY < disk.minLocationNoiseY) mapOutputY = disk.minLocationNoiseY;
+            if (mapOutputZ < disk.minLocationNoiseZ) mapOutputZ = disk.minLocationNoiseZ;
 
             if (disk.decreaseXZNoiseByDistance)
             {
-                noiseX = Random.Range(-mapOutputXZ, mapOutputXZ);
-                noiseZ = Random.Range(-mapOutputXZ, mapOutputXZ);
+                noiseX = Random.Range(-mapOutputX, mapOutputZ);
             }
             else
             {
-                noiseX = Random.Range(-disk.locationNoiseXZ, disk.locationNoiseXZ);
-                noiseZ = Random.Range(-disk.locationNoiseXZ, disk.locationNoiseXZ);
+                noiseX = Random.Range(-disk.locationNoiseX, disk.locationNoiseX);
             }
 
             if (disk.decreaseYNoiseByDistance)
@@ -364,6 +202,15 @@ public class GalaxyFactory : MonoBehaviour
             else
             {
                 noiseY = Random.Range(-disk.locationNoiseY, disk.locationNoiseY);
+            }
+
+            if (disk.decreaseXZNoiseByDistance)
+            {
+                noiseZ = Random.Range(-mapOutputZ, mapOutputZ);
+            }
+            else
+            {
+                noiseZ = Random.Range(-disk.locationNoiseZ, disk.locationNoiseZ);
             }
 
             float pointXAfterNoise = pointX + noiseX;
